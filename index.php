3999cfc328719522cf99323d1abd4cad9541e4eb
Alex <alex@wearenotmachines.com>
2015-03-01 22:04:12 +0000

    Stickers can be added and removed

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use WeAreNotMachines\Kanban\KanbanBoard;
 use WeAreNotMachines\Kanban\KanbanBoardUser;
 use WeAreNotMachines\Kanban\KanbanBoardClient;
 use WeAreNotMachines\Kanban\KanbanBoardProject;
+use WeAreNotMachines\Kanban\KanbanBoardSticker;
 use WeAreNotMachines\TogglClient\TogglClient;
 
 $kb = new KanbanBoard("data.json");
@@ -15,11 +16,14 @@ $kb = new KanbanBoard("data.json");
 $router = new WeAreNotMachines\Utilities\Router;
 
 $router->add("/", function() use ($kb) {
-	print_r($kb->toArray());
+	$toggl = new TogglClient($kb->getUserApiToken("alex.callard@itrm.co.uk"));
+	$kb->setUsers($toggl->getAllUsers());
+	print_r($kb->getUsers());
 });
 
 $router->add("/cache-data", function() use ($kb) {
-	$toggl = new TogglClient($kb->getUserApiToken("alex.callard@itrm.co.uk"));
+	$toggl = new TogglClient("f3862117ce3d14541c6e4ed5a965a80d");
+	$kb->setUsers($toggl->getAllUsers());
 	$kb->setClients($toggl->getAllClients());
 	$kb->setProjects($toggl->getAllProjects());
 	echo $kb->save();
@@ -33,9 +37,28 @@ $router->add("/active-projects", function() use ($kb) {
 	echo json_encode($output, JSON_PRETTY_PRINT);
 });
 
+$router->add("/project/{:id}/addSticker", function($id) use ($kb) {
+	$stickerData = $_POST['sticker'];
+	$kb->addStickerToProject(KanbanBoardSticker::fromArray($stickerData), $id);
+	echo $kb->getProject($id)->toJSON();
+	$kb->save();
+});
+
+$router->add("/project/{:id}/removeSticker", function($id) use ($kb) {
+	$stickerData = $_POST['sticker'];
+	$kb->removeStickerFromProject(KanbanBoardSticker::fromArray($stickerData), $id);
+	echo $kb->getProject($id)->toJSON();
+	$kb->save();
+});
+
 /**
  * Toggl API lookup methods follow
  */
+
+$router->add("/list-users", function() use ($kb) {
+	$toggl = new TogglClient($kb->getUserApiToken("alex.callard@itrm.co.uk"));
+	echo json_encode($toggl->getAllUsers(), JSON_PRETTY_PRINT);
+});
 
 $router->add("/list-clients", function() use ($kb) {
 	$toggl = new TogglClient($kb->getUserApiToken("alex.callard@itrm.co.uk"));
